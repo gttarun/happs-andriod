4cb7741eb8b0c9e4c3674ec50baef7976b01b703
DESKTOP-HM8939P\Ali <ali.h.momin@gmail.com>
2016-10-12 20:18:00 -0500

    got facebook login working

--- Happs/app/src/main/java/ee364e/happs/DefineEvent.java
+++ Happs/app/src/main/java/ee364e/happs/DefineEvent.java
@@ -2,8 +2,8 @@ package ee364e.happs;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
