a76e75c0cb4129b8d16b17856748002a53a760f0
chan-young Kim <cykim07nd@yahoo.com>
2016-10-22 16:30:15 -0500

    progress made, working on camera, currently able to post and pull events

---
+++ Happs/app/src/main/java/ee364e/happs/CameraActivity.java
@@ -0,0 +1,55 @@
+package ee364e.happs;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void onCamera(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+}
--- Happs/app/src/main/java/ee364e/happs/MainActivity.java
+++ Happs/app/src/main/java/ee364e/happs/MainActivity.java
@@ -1,23 +1,31 @@
 package ee364e.happs;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.facebook.FacebookSdk;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+
 
 
 import org.json.JSONException;
@@ -40,6 +48,8 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -59,12 +69,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
                     .addApi(AppIndex.API).build();
         }
         super.onCreate(savedInstanceState);
-       // new LongRunningPostIO().execute();
         new LongRunningGetIO().execute();
-
         setContentView(R.layout.activity_main);
     }
 
@@ -75,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         intent.putExtra("long", longitude);
         intent.putExtra("lat", latitude);
         startActivity(intent);
+       new GetAddress().execute();
     }
 
     protected void onStart() {
@@ -215,135 +226,56 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
 
 
-
-
-
-
-    private class LongRunningPostIO extends AsyncTask<Void, Void, String> {
-        protected String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-            Reader reader = null;
-            reader = new InputStreamReader(stream, "UTF-8");
-            char[] buffer = new char[len];
-            reader.read(buffer);
-            return new String(buffer);
-        }
+    private class GetAddress extends AsyncTask<Void, Void, String> {
 
         @Override
         protected String doInBackground(Void... params) {
-            InputStream is = null;
-            // Only display the first 500 characters of the retrieved
-            // web page content.
-            int len = 500;
-
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> list = null;
+            String currentAddress = null;
             try {
-                URL url = null;
-                try {
-                    url = new URL(URL);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
+                list = geocoder.getFromLocation(latitude, longitude, 1);
+                if(list != null && list.size() > 0) {
+                    Address address = list.get(0);
+                    // sending back first address line and locality
+                    currentAddress = address.getAddressLine(0) + ", " + address.getLocality();
                 }
-
-
-                HttpURLConnection conn = null;
-                try {
-                    conn = (HttpURLConnection) url.openConnection();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-                conn.setReadTimeout(10000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-
-
-                try {
-                    conn.setRequestMethod("POST");
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                }
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                conn.setRequestProperty("Content-Type","application/json");
-                // Starts the query
-                try {
-                    conn.connect();
-                    JSONObject jsonParam = new JSONObject();
-                    try {
-                        jsonParam.put("event_name", "random");
-                        jsonParam.put("time", "2016-06-29T09:57:00Z");
-                        jsonParam.put("longitude", "-35");
-                        jsonParam.put("latitude", "68");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream ());
-                    os.writeBytes(jsonParam.toString());
-                    os.flush ();
-                    os.close ();
-                    int response = conn.getResponseCode();
-                    String contentAsString = readIt(is, len);
-                    return contentAsString;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // Convert the InputStream into a string
-
-
-                return "retrieval failed";
-
-                // Makes sure that the InputStream is closed after the app is
-                // finished using it.
-            } finally {
-                if (is != null) {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                    .getCurrentPlace(mGoogleApiClient, null);
+            result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+                @Override
+                public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                    StringBuilder address = new StringBuilder();
+                    for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                String place = placeLikelihood.getPlace().getName().toString();
+                                String place2 = placeLikelihood.getPlace().getAddress().toString();
+                                String like = Float.toString(placeLikelihood.getLikelihood());
+                        address.append(place + " " + like + " " + place2 +  "\n");
+                    }
+                    likelyPlaces.release();
+                    String result = address.toString();
+                    TextView textView = (TextView) findViewById(R.id.textView2);
+                    textView.setText(result);
+                }
+            });
+            return currentAddress;
         }
 
 
 
-
-        protected void onPostExecute(String results) {
-            /*if (results != null) {
-                result = results;
-                TextView textView = (TextView) findViewById(R.id.textView2);
+        protected void onPostExecute(String currentAddress) {
+            if (currentAddress != null) {
+                result = currentAddress;
+                TextView textView = (TextView) findViewById(R.id.textView);
                 textView.setText(result);
-
             }
 
 
-            Button b = (Button)findViewById(R.id.button2);
-
-
-            b.setClickable(true);*/
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
