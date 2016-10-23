a6d65c62fd65a27995bd4f8eab3c9729b87d1a68
cykim07nd <chanyoung.kim95@gmail.com>
2016-10-23 14:44:58 -0500

    testing uploading

--- Happs/app/src/main/java/ee364e/happs/CameraActivity.java
+++ Happs/app/src/main/java/ee364e/happs/CameraActivity.java
@@ -2,9 +2,13 @@ package ee364e.happs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,12 +16,35 @@ import android.widget.ImageView;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class CameraActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     ImageView imageView,imageView2;
-
-    Bitmap photo;
+    File finalFile;
+    private final String URL = "http://teamhapps.herokuapp.com/api/images/";
+    private final String boundary = "xxxxx";
     Button crop;
+    String lineEnd = "\r\n";
+    String twoHyphens = "--";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +75,118 @@ public class CameraActivity extends Activity {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
+                finalFile = new File(resultUri.getPath());
                 imageView.setImageURI(resultUri);
+                new LongRunningPostPicture().execute();
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
     }
 
+
+    private class LongRunningPostPicture extends AsyncTask<Void, Void, String> {
+        protected String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            int bytesRead, bytesAvailable, bufferSize;
+            byte[] buffer;
+            int maxBufferSize = 1*1024*1024;
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+            try {
+                URL url = null;
+                try {
+                    url = new URL(URL);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+
+
+                try {
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Content-Type","multipart/form-data boundary=" + boundary);
+                // Starts the query
+                try {
+                    conn.connect();
+                    FileInputStream fileInputStream = new FileInputStream(finalFile);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream ());
+                    os.writeBytes(twoHyphens + boundary + lineEnd);
+                    os.writeBytes("Content-Disposition: form-data; name=" +  "uploadedpicture"  + ";filename=" + finalFile.getName()  + lineEnd);
+                    os.writeBytes(lineEnd);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    while (bytesRead > 0){
+                        os.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    }
+                    os.writeBytes(lineEnd);
+                    os.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+                    fileInputStream.close();
+                    os.flush ();
+                    os.close ();
+                    is = new DataInputStream( conn.getInputStream() );
+                    int response = conn.getResponseCode();
+                    return "success";
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Convert the InputStream into a string
+
+
+                return "retrieval failed";
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    try {
+                        String contentAsString = readIt(is, len);
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+
+
+        protected void onPostExecute(String results) {
+
+        }
+    }
 
 }
