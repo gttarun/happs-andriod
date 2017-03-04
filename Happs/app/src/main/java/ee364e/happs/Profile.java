e9fedb42bf2ff36f6f60e7756ecc960a7650f223
DESKTOP-HM8939P\Ali <ali.h.momin@gmail.com>
2017-03-04 11:32:59 -0600

    changes made by Ali, implementing profile view

---
+++ Happs/app/src/main/java/ee364e/happs/Profile.java
@@ -0,0 +1,69 @@
+package ee364e.happs;
+
+import android.graphics.Picture;
+import android.media.Image;
+import android.net.Uri;
+import android.net.wifi.ScanResult;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ali on 2/13/2017.
+ */
+
+public class Profile {
+
+    private String name;
+    private String userName;
+    private String description;
+    private ArrayList<String> friends;
+    private ArrayList<Event> events;
+    private Uri profilePic;
+
+    public ArrayList<Event> getEvents() {return events;}
+
+    public void setEvents(ArrayList<Event> events) {this.events = events;}
+
+    public ArrayList<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<String> friends) {
+        this.friends = friends;
+    }
+
+    public Uri getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(Uri profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
